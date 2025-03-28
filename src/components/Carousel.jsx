2050c83d9fffea4f70c244d6bcb7b191cfbdbed3
ItsMAX0112 <aayushvats361@gmail.com>
2025-03-29 00:45:26 +0530

    carousel nav

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -11,15 +11,30 @@ const images = Object.values(importImages).map((module) => module.default);
 
 function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isAutoPlaying, setIsAutoPlaying] = useState(true);
 
+  // Auto-play effect
   useEffect(() => {
-    if (images.length > 0) {
+    if (images.length > 0 && isAutoPlaying) {
       const interval = setInterval(() => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
       }, 3000); // Change image every 3 seconds
       return () => clearInterval(interval);
     }
-  }, []);
+  }, [isAutoPlaying, images.length]);
+
+  // Navigation handlers
+  const goToPrevious = () => {
+    setIsAutoPlaying(false); // Stop auto-play on manual navigation
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNext = () => {
+    setIsAutoPlaying(false); // Stop auto-play on manual navigation
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
 
   if (images.length === 0) {
     return <div>No images found in carousel folder</div>;
@@ -27,7 +42,13 @@ function Carousel() {
 
   return (
     <div className="carousel">
+      <button className="carousel-button prev" onClick={goToPrevious}>
+        &#9664; {/* Left arrow */}
+      </button>
       <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      <button className="carousel-button next" onClick={goToNext}>
+        &#9654; {/* Right arrow */}
+      </button>
     </div>
   );
 }
