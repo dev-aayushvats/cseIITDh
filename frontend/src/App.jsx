eeb4332f74e57dab8a57a093aa46ba577d166911
swasti-sj <jainswasti2004@gmail.com>
2025-04-13 00:50:20 +0530

    added carousel in home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,35 @@
+import React from 'react';
 import Topbar from './components/Topbar/Topbar';
 import Navbar from './components/Navbar/Navbar';
 import Carousel from './components/Carousel/Carousel';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// import Academics from './pages/Academics';
-// import Admissions from './pages/Admissions';
-// import Research from './pages/Research';
-// ...other page imports
 
 function App() {
   return (
     <>
       <Topbar />
-      
-      {/* Use Flexbox for layout */}
-      <div className="flex">
-        {/* Navbar Section */}
+
+      {/* Push content below fixed Topbar */}
+      <div className="pt-[90px] flex">
+        {/* Sidebar/Navbar */}
         <div className="w-1/4">
           <Navbar />
         </div>
 
-        {/* Main content and Carousel Section */}
+        {/* Main content */}
         <div className="flex-1 p-4">
-          {/* Carousel will be at the top, next to Navbar */}
-          <div className="mb-8 pt-[80px]">
+          {/* Carousel at the top */}
+          <div className="mb-80">
             <Carousel />
           </div>
 
-          {/* Main content area with routing */}
+          {/* Routing area */}
           <main>
+          
+
             <Routes>
               <Route path="/" element={<Home />} />
-              {/* <Route path="/academics" element={<Academics />} />
-              <Route path="/admissions" element={<Admissions />} />
-              <Route path="/research" element={<Research />} /> */}
-              {/* Add more routes here */}
             </Routes>
           </main>
         </div>
--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
-import logo1 from '../../assets/carousel-images/Academic-About.e6a203bff9b9d7dd0d81.jpg'
-import logo2 from '../../assets/carousel-images/banner-01.jpg'
-import logo3 from '../../assets/carousel-images/campus.jpg'
+import logo1 from '../../assets/carousel-images/Academic-About.e6a203bff9b9d7dd0d81.jpg';
+import logo2 from '../../assets/carousel-images/banner-01.jpg';
+import logo3 from '../../assets/carousel-images/campus.jpg';
 
 const Carousel = () => {
-  // Create a state to track the active slide
   const [currentSlide, setCurrentSlide] = useState(0);
   const images = [logo1, logo2, logo3]; // Store the images in an array
 
   const handleNext = () => {
-    // Set next slide index, reset to 0 if at the last slide
     setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
   };
 
   const handlePrev = () => {
-    // Set previous slide index, wrap around to last if at the first slide
     setCurrentSlide((prevSlide) => (prevSlide - 1 + images.length) % images.length);
   };
 
   return (
-    <div id="carouselExample" className="relative" data-carousel="slide">
+    <div
+      id="carouselExample"
+      className="relative mx-auto max-w-3xl h-40" // Limited width and height
+      data-carousel="slide"
+    >
       {/* Carousel indicators */}
       <div className="absolute top-0 left-0 z-30 flex space-x-3 justify-center p-4">
         {images.map((_, index) => (
@@ -27,18 +28,18 @@ const Carousel = () => {
             key={index}
             type="button"
             className={`w-3 h-3 bg-white rounded-full dark:bg-gray-800 ${currentSlide === index ? 'bg-blue-500' : ''}`}
-            onClick={() => setCurrentSlide(index)} // Go to specific slide when clicked
+            onClick={() => setCurrentSlide(index)}
             aria-label={`Slide ${index + 1}`}
           ></button>
         ))}
       </div>
 
       {/* Carousel items */}
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <div className="relative overflow-hidden rounded-lg">
         <div className="duration-700 ease-in-out">
           <img
-            src={images[currentSlide]} // Display the current active image
-            className="block w-full h-full object-cover"
+            src={images[currentSlide]}
+            className="block w-full h-full object-cover" // Make images fill the container
             alt={`Image ${currentSlide + 1}`}
           />
         </div>
@@ -48,7 +49,7 @@ const Carousel = () => {
       <button
         type="button"
         className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={handlePrev} // Previous button functionality
+        onClick={handlePrev}
       >
         <span className="inline-flex items-center justify-center w-10 h-10 text-white rounded-full bg-black/30 group-hover:bg-black/50 group-focus:ring-4 group-focus:ring-black/50">
           <svg
@@ -74,7 +75,7 @@ const Carousel = () => {
       <button
         type="button"
         className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={handleNext} // Next button functionality
+        onClick={handleNext}
       >
         <span className="inline-flex items-center justify-center w-10 h-10 text-white rounded-full bg-black/30 group-hover:bg-black/50 group-focus:ring-4 group-focus:ring-black/50">
           <svg
